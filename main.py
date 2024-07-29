ff3c46af985c0b1ddd73b920acacbe1bd5c258e4
Michael Cather <mgcather07@gmail.com>
2024-07-29 16:56:31 -0500

    new card

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 import logging
 from webex_bot.webex_bot import WebexBot
+from webex_bot.commands.echo import EchoCommand
+from webex_bot.commands.help import HelpCommand
 from location import Location
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -11,33 +14,16 @@ log = logging.getLogger(__name__)
 # we'll retrieve that here:
 webex_token = os.environ["WEBEX_ACCESS_TOKEN"]
 
-# Define approved users
-approved_users = ['MCather@drummondco.com']
+# Create an instance of the Webex bot
+bot = WebexBot(webex_token)
 
+# Register custom commands with the bot
+location_command = Location()
 
-# Overriding the WebexBot class to add logging for user checks
-class CustomWebexBot(WebexBot):
-    def __init__(self, access_token, **kwargs):
-        super().__init__(access_token, **kwargs)
+# Add commands to the bot
+bot.add_command(location_command)
+bot.add_command(EchoCommand())
 
-    def on_message(self, message):
-        user_email = message.personEmail
-        log.info(f"Incoming message from {user_email}")
-
-        if user_email in self.approved_users:
-            log.info(f"User {user_email} is approved.")
-            super().on_message(message)
-        else:
-            log.warning(f"User {user_email} is not approved.")
-            response = "You are not authorized to use this bot."
-            self.teams.messages.create(roomId=message.roomId, text=response)
-
-
-# Create an instance of the custom bot
-custom_bot = CustomWebexBot(webex_token, approved_users=approved_users)
-
-# Register custom command with the bot
-custom_bot.add_command(Location())
 
 # Connect to Webex & start bot listener
-custom_bot.run()
+bot.run()
